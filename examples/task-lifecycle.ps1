808bc1714de554fb60e77368eda718d5a63068bd
ZZZe1415 <mupengze2024@outlook.com>
2026-08-23 21:40:55 +0800

    Add examples/task-lifecycle.ps1

---
+++ examples/task-lifecycle.ps1
@@ -0,0 +1,10 @@
+$petRoot = Split-Path -Parent $PSScriptRoot
+
+& "$petRoot\Set-FufuState.ps1" -State working -Message '正在处理任务…'
+Start-Sleep -Seconds 2
+
+& "$petRoot\Set-FufuState.ps1" -State waiting -Message '请确认是否继续。'
+Start-Sleep -Seconds 2
+
+& "$petRoot\Set-FufuState.ps1" -State done -Message '任务完成啦！'
+
